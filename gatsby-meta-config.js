5ae0571f1c381562783bea1d5db593e6dd4d1be6
jieun-p974 <jieun.p974@gmail.com>
2022-08-09 20:31:03 +0900

    수정 시작

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,26 +1,26 @@
 module.exports = {
-  title: `zoomkoding.com`,
-  description: `줌코딩의 개발일기`,
+  title: `Chion's blog`,
+  description: `뜨개질하는 개발자의 일기장`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://www.zoomkoding.com`,
+  siteUrl: `https://www.chionss.com`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: ``, // `zoomkoding/zoomkoding-gatsby-blog`,
+      repo: ``, // 댓글 설정
     },
   },
   ga: '0', // Google Analytics Tracking ID
   author: {
-    name: `정진혁`,
+    name: `박지은`,
     bio: {
-      role: `개발자`,
+      role: `프론트엔드 개발자`,
       description: ['사람에 가치를 두는', '능동적으로 일하는', '이로운 것을 만드는'],
       thumbnail: 'sample.png', // Path to the image in the 'asset' folder
     },
     social: {
-      github: ``, // `https://github.com/zoomKoding`,
-      linkedIn: ``, // `https://www.linkedin.com/in/jinhyeok-jeong-800871192`,
-      email: ``, // `zoomkoding@gmail.com`,
+      github: `https://github.com/jieun-p974`,
+      linkedIn: ``,
+      email: `jieun.p974@gmail.com`,
     },
   },
 
@@ -44,11 +44,11 @@ module.exports = {
       // ========================================================
       {
         date: '2021.02 ~',
-        activity: '개인 블로그 개발 및 운영',
+        activity: '개발 일기 작성 및 취미 기록',
         links: {
-          post: '/gatsby-starter-zoomkoding-introduction',
-          github: 'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
-          demo: 'https://www.zoomkoding.com',
+          post:'', //'/gatsby-starter-zoomkoding-introduction',
+          github:'', //'https://github.com/zoomkoding/zoomkoding-gatsby-blog',
+          demo:'' //'https://www.zoomkoding.com',
         },
       },
     ],
